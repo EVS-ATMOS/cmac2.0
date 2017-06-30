f11f80a948991908b55bc4261493b237319deb67
Zachary Sherman <zsherman@anl.gov>
2017-06-30 11:23:48 -0500

    MAINT: Removed imports of private functions.

--- cmac/__init__.py
+++ cmac/__init__.py
@@ -16,10 +16,7 @@ Functions
     snr_and_sounding
     get_texture
     cum_score_fuzzy_logic
-    fix_rain_above_bb
     do_my_fuzz
-    extract_unmasked_data
-    csu_to_field
     return_csu_kdp
     retrieve_qvp
 
@@ -29,10 +26,8 @@ Functions
 # will change on the decision of which functions are private or not. 
 from .cmac_xsapr import cmac
 from .cmac_quicklooks import quicklooks
-from .processing_code import snr_and_sounding
+from .processing_code import snr_and_sounding, do_my_fuzz
 from .processing_code import get_texture, cum_score_fuzzy_logic
-from .processing_code import fix_rain_above_bb, do_my_fuzz
-from .processing_code import extract_unmasked_data, csu_to_field
 from .processing_code import return_csu_kdp, retrieve_qvp
 
 # __all__ = [s for s in dir() if not s.startswith('_')]
