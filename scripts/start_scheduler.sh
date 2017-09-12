ee09500bee16fc2d5591b36edc305a217c72d0f4
zssherman <shermanx1991@gmail.com>
2017-09-12 10:53:27 -0400

    MAINT: Added zach sherman user env information and removed bokeh because it was causing failures.

--- scripts/start_scheduler.sh
+++ scripts/start_scheduler.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+export PATH=/home/zsherman/anaconda3/envs/cmac_env/bin:$PATH
 source activate cmac_env
-export PATH=/home/rjackson/anaconda3/bin:$PATH
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export TMPDIR=$2
 export MPLBACKEND="agg"
-dask-scheduler --port=8786 --scheduler-file=$1 --local-directory=$2
+dask-scheduler --port=8786 --scheduler-file=$1 --local-directory=$2 --no-bokeh
