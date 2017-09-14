99334ef957595b65d8924e01a0dfa91cc82b9f5f
Jackson R C <rjackson@cades-extlogin01.ornl.gov>
2017-09-14 13:21:39 -0400

    ADD: Dask for clutter generation

--- cmac/xsapr_clutter.py
+++ cmac/xsapr_clutter.py
@@ -50,13 +50,10 @@ def xsapr_clutter(files, clutter_thresh_min=0.0002,
             radar = pyart.io.read(file)
             reflect_array = deepcopy(radar.fields['reflectivity']['data'])
             del radar
-<<<<<<< HEAD
+
             if(reflect_array.shape == first_shape):
                 return reflect_array.filled(fill_value=np.nan)
-        except:
-=======
         except (TypeError, RuntimeError) as e:
->>>>>>> 3274aa2a4a67d71144fe6df0078d92e3a075c779
             print(file + ' is corrupt...skipping!')
             
         return np.nan*np.zeros(first_shape)
@@ -75,7 +72,7 @@ def xsapr_clutter(files, clutter_thresh_min=0.0002,
                 if reflect_array.shape == first_shape:
                     run_stats.push(reflect_array)
                 del radar
-            except TypeError:
+            except TypeError, RuntimeError:
                 print(file + ' is corrupt...skipping!')
                 continue
         mean = run_stats.mean()
@@ -90,7 +87,7 @@ def xsapr_clutter(files, clutter_thresh_min=0.0002,
                 reflect_array = radar.fields['reflectivity']['data']
                 first_shape = reflect_array.shape
                 clutter_radar = radar
-            except TypeError:
+            except TypeError, RuntimeError:
                 i = i + 1
                 print(file + ' is corrupt...skipping!')
                 continue
